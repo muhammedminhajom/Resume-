8ebb42169e9e344b3751a12ba13de070751f9c76
Muhammed Minhaj <muhammedminhajom@gmail.com>
2026-09-12 22:46:32 +0530

    feat: add Vercel Analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
@@ -26,6 +27,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <ThemeProvider>
         <AuthProvider>
           <App />
+          <Analytics />
         </AuthProvider>
       </ThemeProvider>
     </BrowserRouter>
